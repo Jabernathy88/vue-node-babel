e9fe4a2b11b633ab20af41a6ced2eb034409300a
Jeremy Abernathy <j.e.abernathy@gmail.com>
2019-05-28 17:08:28 -0400

    Complete GET method with v-bind on text input.

---
+++ client/src/components/TodoItem.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="todo-item pb-1" v-bind:class="{'is-complete':todo.completed}">
+    <b-input-group>
+      <b-input-group-prepend is-text>
+        <input type="checkbox" aria-label="Checkbox for following text input">
+      </b-input-group-prepend>
+      <!-- add the title as placeholder -->
+        <b-form-input class="w-50" v-bind:placeholder="todo.title"></b-form-input>
+        <b-form-input type="date"></b-form-input>
+      <b-input-group-append class="">
+        <b-button variant="outline-danger">x</b-button>
+      </b-input-group-append>
+
+      
+      <!-- <input type="checkbox" v-on:change="markComplete" />
+      {{todo.title}}
+      <b-button @click="$emit('del-todo', todo.id)" class="del">x</b-button> -->
+    </b-input-group>
+
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TodoItem",
+  props: ["todo"],
+  methods: {
+    // markComplete() {
+    //   this.todo.completed = !this.todo.completed;
+    // }
+  }
+}
+</script>
+
+<style scoped>
+  /* .todo-item {
+    background: #f4f4f4;
+    padding: 10px;
+    border-bottom: 1px #ccc dotted;
+  }
+  .is-complete {
+    text-decoration: line-through;
+  }
+  .del {
+    background: #ff0000;
+    color: #fff;
+    border: none;
+    padding: 5px 9px;
+    borderius: 50%;
+    cursor: pointer;
+    float: right;
+  } */
+</style>
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,20 +1,17 @@
 <template>
   <div>
     <b-container fluid class="">
-      <b-col cols="4" class="text-center">
-      </b-col>
-      <b-col>
-        <h1>Welcome to Vue.js</h1>
-        <p class="lead">
+      <b-row class="pt-3 px-3 d-block">
+        <h1 class="d-block">Welcome to Vue.js</h1>
+        <p class="lead d-block">
           For a guide and recipes on how to configure / customize this project,<br>
           check out the <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
         </p>
-      </b-col>
+      </b-row>
     <!-- <img class="vue-logo" alt="Vue logo" src="../assets/logo.png"> -->
-  </b-container>
 
-  <b-container>
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo" /> -->
+    <TodosList v-bind:todos="todos" />
   </b-container>
   </div>
 
@@ -22,12 +19,43 @@
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import TodosList from '@/components/TodosList.vue'
+import axios from 'axios'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    TodosList
+  },
+  data() {
+    return {
+      todos: []
+    }
+  },
+  methods: {
+    // deleteTodo(id) {
+    //   axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
+    //     .then(res => this.todos = this.todos.filter(todo => todo.id !== id))
+    //     .catch(err => console.log(err));
+    // },
+    // addTodo(newTodo) {
+    //   const {title, completed} = newTodo
+    //   axios.post('https://jsonplaceholder.typicode.com/todos', {
+    //     title,
+    //     completed
+    //   })
+    //     .then(res => this.todos = [...this.todos, res.data])
+    //     .catch(err => console.log(err))
+    //   this.todos = [...this.todos]
+    // }
+  },
+  created() {
+    axios.get('http://localhost:3001/api/todos')
+      .then(res => {
+        this.todos = res.data
+        console.log('I am created method response:', res.data)
+        })
+      .catch(err => console.log(err))
   }
 }
 </script>
