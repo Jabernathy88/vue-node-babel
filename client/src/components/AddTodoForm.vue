b479831b97ac6a42d3a8da4085270380c4d097b4
Jeremy Abernathy <j.e.abernathy@gmail.com>
2019-05-29 14:35:25 -0400

    Install vuejs-datepicker

--- client/src/components/AddTodoForm.vue
+++ client/src/components/AddTodoForm.vue
@@ -1,38 +1,39 @@
 <template>
-
-    <b-form-row @submit="createNewTodo" >
-      <b-form-group class="w-100" description="Create a new task and schedule when it's due.">
-        <b-input-group prepend="Title">
-          <b-form-input type="text" v-model="title" name="title" placeholder=" Name your next task!" />
-        </b-input-group>
-        <b-form-row class="mt-1">
-          <b-col cols="8" sm="5" md="4">
-            <b-input-group class="" prepend="Due">
-              <b-form-input v-bind:id="'todo.id'" type="date" class=""></b-form-input>
-            </b-input-group>
-          </b-col >
-          <b-col cols="3" sm="3" md="2">
-            <b-button block class="" type="submit" variant="outline-success" >
-              Submit
-            </b-button>
-          </b-col>
-        </b-form-row>
-
-
-
-
-      </b-form-group>
-
-    </b-form-row>  
+  <b-form-row @submit="createNewTodo" >
+    <b-form-group class="w-100" description="Create a new task and schedule when it's due.">
+      <b-input-group prepend="Title">
+        <b-form-input type="text" v-model="title" name="title" placeholder=" Name your next task!" />
+      </b-input-group>
+      <b-form-row class="mt-1">
+        <b-col cols="8" sm="5" md="4">
+          <b-input-group class="" prepend="Due">
+            <b-form-input v-bind:id="'todo.id'" type="date" class=""></b-form-input>
+          </b-input-group>
+        </b-col >
+        <b-col cols="3" sm="3" md="2">
+          <b-button block class="" type="submit" variant="outline-success" >
+            Submit
+          </b-button>
+        </b-col>
+      </b-form-row>
+      <!-- custom datepicker
+      <datepicker></datepicker> -->
+    </b-form-group>
+  </b-form-row>  
 </template>
 
 <script>
+  import Datepicker from 'vuejs-datepicker'
+
   export default {
     name: "AddTodoForm",
     data() {
       return {
         title: ''
       }
+    },
+    components: {
+      Datepicker
     },
     methods: {
       createNewTodo(e) {
