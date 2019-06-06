e7d67afb2ab1684ef3b7b22bf82568821475f65b
Jeremy Abernathy <j.e.abernathy@gmail.com>
2019-06-06 11:09:10 -0400

    Misc cleanup.

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -1,17 +1,12 @@
 <template>
   <b-navbar toggleable="lg" type="dark" variant="dark">
     <b-navbar-brand href="#">NavBar</b-navbar-brand>
-
     <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-
     <b-collapse id="nav-collapse" is-nav>
       <b-navbar-nav>
         <b-nav-item to="/">Home</b-nav-item>
         <b-nav-item to="/about">About</b-nav-item>
       </b-navbar-nav>
-
-      <!-- Removed right-aligned  -->
-
     </b-collapse>
   </b-navbar>
 </template>
@@ -22,7 +17,5 @@ export default {
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
 </style>
--- client/src/components/TodoItem.vue
+++ client/src/components/TodoItem.vue
@@ -6,14 +6,9 @@
           <input type="checkbox" aria-label="Checkbox for following text input" v-on:change="markComplete">
         </b-input-group-prepend>
         <b-input-group-text class="w-50">{{todo.title}} </b-input-group-text>
-
-        <!-- <b-form-input is-text class="w-25 border-right-0" v-bind:placeholder="todo.title"></b-form-input> -->
         <b-input-group-append class="">
           <b-button variant="outline-danger" @click="$emit('del-todo', todo.id)" class="del"><i class="fas fa-times"></i></b-button>
         </b-input-group-append>
-        <!-- <input type="checkbox" v-on:change="markComplete" />
-        {{todo.title}}
-        <b-button @click="$emit('del-todo', todo.id)" class="del">x</b-button> -->
       </b-input-group>
     </b-form-row>
   </div>
